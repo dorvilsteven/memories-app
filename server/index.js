9e76e0695eba3c940d5fd1d991536cd05c225832
Steven Dorvil <dorvilsteven@gmail.com>
2022-03-26 20:05:50 -0400

    updated index.js to include mongodb cloud database using connection url, nodemon working and server running

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'; 
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://dorvilsteven:Dontneed123@cluster0.mfj9f.mongodb.net/memories-app?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3005;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// Must figure out this bug, Error: `useFindAndModify` is an invalid option.
+// mongoose.set('useFindAndModify', false);
