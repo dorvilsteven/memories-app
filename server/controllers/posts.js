51b975a44750d18fa7034a7dab338137db43eb1c
Steven Dorvil <dorvilsteven@gmail.com>
2022-04-07 21:50:22 -0400

    finish with inital get post and create post logic in the controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,23 @@
 import PostData from '../models/postData.js';
 
-export const getPosts = (req, res) => {
-    res.send('welcome to memories post\'s page');
+export const getPosts = async (req, res) => {
+    // res.send('welcome to memories post\'s page');
+    try {
+        const posts = await PostData.find();
+        console.log(posts);
+        res.status(200).json(posts)
+    } catch(error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export const createPost = (req, res) => {
-    res.send('post creation');
+export const createPost = async (req, res) => {
+    // res.send('post creation');
+    const post = req.body;
+    const newPost = new PostData(post);
+    try {
+        await newPost.save();
+    } catch(error) {    
+        res.status(404).json({ message: error.message });
+    }
 };
